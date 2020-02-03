190abacf28d1584988f4e159a3a0e6c6458d1c0e
Yuri Roubinsky <chaosus89@gmail.com>
2020-02-03 18:14:41 +0300

    Restores correct window position after fullscreen toggling off on Linux
    (cherry picked from commit 90f00c0813b8335605c81b5d5063922efe5519e3)

--- platform/x11/os_x11.cpp
+++ platform/x11/os_x11.cpp
@@ -1435,7 +1435,11 @@ void OS_X11::set_window_fullscreen(bool p_enabled) {
 		// Restore
 		set_window_maximized(false);
 	}
-
+	if (!p_enabled) {
+		set_window_position(last_position_before_fs);
+	} else {
+		last_position_before_fs = get_window_position();
+	}
 	current_videomode.fullscreen = p_enabled;
 }
 
@@ -3504,4 +3508,5 @@ OS_X11::OS_X11() {
 	window_focused = true;
 	xim_style = 0L;
 	mouse_mode = MOUSE_MODE_VISIBLE;
+	last_position_before_fs = Vector2();
 }
