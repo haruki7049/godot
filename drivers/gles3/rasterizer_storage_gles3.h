27cb1fbdfc8fc3ae041e20665448dc37b2ba731a
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-10-20 18:58:07 +0200

    Add a debanding property to Viewport for GLES3
    It can be enabled in the Project Settings
    (`rendering/quality/filters/use_debanding`). It's disabled
    by default as it has a small performance impact and can make
    PNG screenshots much larger (due to how dithering works).
    
    As a result, it should be enabled only when banding is noticeable enough.
    
    Since debanding requires a HDR viewport to work, it's only supported
    in the GLES3 backend.

--- drivers/gles3/rasterizer_storage_gles3.h
+++ drivers/gles3/rasterizer_storage_gles3.h
@@ -1393,6 +1393,7 @@ public:
 		bool used_in_frame;
 		VS::ViewportMSAA msaa;
 		bool use_fxaa;
+		bool use_debanding;
 
 		RID texture;
 
@@ -1404,7 +1405,8 @@ public:
 				height(0),
 				used_in_frame(false),
 				msaa(VS::VIEWPORT_MSAA_DISABLED),
-				use_fxaa(false) {
+				use_fxaa(false),
+				use_debanding(false) {
 			exposure.fbo = 0;
 			buffers.fbo = 0;
 			external.fbo = 0;
@@ -1433,6 +1435,7 @@ public:
 	virtual void render_target_clear_used(RID p_render_target);
 	virtual void render_target_set_msaa(RID p_render_target, VS::ViewportMSAA p_msaa);
 	virtual void render_target_set_use_fxaa(RID p_render_target, bool p_fxaa);
+	virtual void render_target_set_use_debanding(RID p_render_target, bool p_debanding);
 
 	/* CANVAS SHADOW */
 
