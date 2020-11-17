59897d3f27cec76aee68bf388d177d191a86b3f9
Pedro J. Estébanez <pedrojrulez@gmail.com>
2020-11-18 00:33:39 +0100

    Ignore query to maximize/minimize window on Windows and X11
    This makes these platform behave as MacOS in that regard and also fixes the editor window appearing in some cases even when --no-window has been passed.

--- platform/x11/os_x11.cpp
+++ platform/x11/os_x11.cpp
@@ -1520,6 +1520,9 @@ bool OS_X11::is_window_resizable() const {
 }
 
 void OS_X11::set_window_minimized(bool p_enabled) {
+	if (is_no_window_mode_enabled()) {
+		return;
+	}
 	// Using ICCCM -- Inter-Client Communication Conventions Manual
 	XEvent xev;
 	Atom wm_change = XInternAtom(x11_display, "WM_CHANGE_STATE", False);
@@ -1583,6 +1586,9 @@ bool OS_X11::is_window_minimized() const {
 }
 
 void OS_X11::set_window_maximized(bool p_enabled) {
+	if (is_no_window_mode_enabled()) {
+		return;
+	}
 	if (is_window_maximized() == p_enabled)
 		return;
 
