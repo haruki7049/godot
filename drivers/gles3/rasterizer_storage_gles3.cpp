ef182c88a6f939c46d21c92ae562e1305c64068e
lawnjelly <lawnjelly@gmail.com>
2020-12-23 11:25:00 +0000

    Remove buffer orphaning options project settings
    These were only put in for the betas, in order to test hypotheses for stalling on Macs. It seems that most of the problems in the Mac editor have been solved by fixing the excessive redraw_requests.
    
    As a result no one has reported any results from these options, but in future we will be able to refer users to try the beta versions, so there is no need to include them in the stable release. Indeed they are only likely to cause confusion.

--- drivers/gles3/rasterizer_storage_gles3.cpp
+++ drivers/gles3/rasterizer_storage_gles3.cpp
@@ -8572,8 +8572,6 @@ void RasterizerStorageGLES3::initialize() {
 			}
 		}
 	}
-
-	config.should_orphan = GLOBAL_GET("rendering/options/api_usage_legacy/orphan_buffers");
 }
 
 void RasterizerStorageGLES3::finalize() {
