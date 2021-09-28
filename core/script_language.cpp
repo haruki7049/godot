KleirRampage45/redot-engine

Don't save project settings when not necessary

core/object/script_language.cpp
@@ -275,6 +275,14 @@ void ScriptServer::save_global_classes() {
 		gcarr.push_back(d);
 	}
 
+	Array old;
+	if (ProjectSettings::get_singleton()->has_setting("_global_script_classes")) {
+		old = ProjectSettings::get_singleton()->get("_global_script_classes");
+	}
+	if ((!old.is_empty() || gcarr.is_empty()) && gcarr.hash() == old.hash()) {
+		return;
+	}
+
 	if (gcarr.is_empty()) {
 		if (ProjectSettings::get_singleton()->has_setting("_global_script_classes")) {
 			ProjectSettings::get_singleton()->clear("_global_script_classes");

editor/editor_data.cpp
@@ -942,6 +942,14 @@ void EditorData::script_class_save_icon_paths() {
 		}
 	}
 
+	Dictionary old;
+	if (ProjectSettings::get_singleton()->has_setting("_global_script_class_icons")) {
+		old = ProjectSettings::get_singleton()->get("_global_script_class_icons");
+	}
+	if ((!old.is_empty() || d.is_empty()) && d.hash() == old.hash()) {
+		return;
+	}
+
 	if (d.is_empty()) {
 		if (ProjectSettings::get_singleton()->has_setting("_global_script_class_icons")) {
 			ProjectSettings::get_singleton()->clear("_global_script_class_icons");

haruki7049/godot

(cherry picked from commit 4db47eb32e550fb1f2d8967907b66b2d41f09050)

core/script_language.cpp
@@ -276,6 +276,14 @@ void ScriptServer::save_global_classes() {
 		gcarr.push_back(d);
 	}
 
+	Array old;
+	if (ProjectSettings::get_singleton()->has_setting("_global_script_classes")) {
+		old = ProjectSettings::get_singleton()->get("_global_script_classes");
+	}
+	if ((!old.empty() || gcarr.empty()) && gcarr.hash() == old.hash()) {
+		return;
+	}
+
 	if (gcarr.empty()) {
 		if (ProjectSettings::get_singleton()->has_setting("_global_script_classes")) {
 			ProjectSettings::get_singleton()->clear("_global_script_classes");

editor/editor_data.cpp
@@ -986,6 +986,14 @@ void EditorData::script_class_save_icon_paths() {
 			d[E->get()] = _script_class_icon_paths[E->get()];
 	}
 
+	Dictionary old;
+	if (ProjectSettings::get_singleton()->has_setting("_global_script_class_icons")) {
+		old = ProjectSettings::get_singleton()->get("_global_script_class_icons");
+	}
+	if ((!old.empty() || d.empty()) && d.hash() == old.hash()) {
+		return;
+	}
+
 	if (d.empty()) {
 		if (ProjectSettings::get_singleton()->has_setting("_global_script_class_icons")) {
 			ProjectSettings::get_singleton()->clear("_global_script_class_icons");