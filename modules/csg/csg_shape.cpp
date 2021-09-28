KleirRampage45/redot-engine

Transform CSGShape collision when necessary

modules/csg/csg_shape.cpp
@@ -511,6 +511,12 @@ void CSGShape3D::_notification(int p_what) {
 		_make_dirty();
 	}
 
+	if (p_what == NOTIFICATION_TRANSFORM_CHANGED) {
+		if (use_collision && is_root_shape() && root_collision_instance.is_valid()) {
+			PhysicsServer3D::get_singleton()->body_set_state(root_collision_instance, PhysicsServer3D::BODY_STATE_TRANSFORM, get_global_transform());
+		}
+	}
+
 	if (p_what == NOTIFICATION_LOCAL_TRANSFORM_CHANGED) {
 		if (parent) {
 			parent->_make_dirty();

haruki7049/godot

(cherry picked from commit fc8574fbac1073f4d68197dc8a2b3504bfc8970d)

modules/csg/csg_shape.cpp
@@ -517,6 +517,12 @@ void CSGShape::_notification(int p_what) {
 		_make_dirty();
 	}
 
+	if (p_what == NOTIFICATION_TRANSFORM_CHANGED) {
+		if (use_collision && is_root_shape() && root_collision_instance.is_valid()) {
+			PhysicsServer::get_singleton()->body_set_state(root_collision_instance, PhysicsServer::BODY_STATE_TRANSFORM, get_global_transform());
+		}
+	}
+
 	if (p_what == NOTIFICATION_LOCAL_TRANSFORM_CHANGED) {
 
 		if (parent) {