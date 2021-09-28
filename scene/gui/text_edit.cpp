KleirRampage45/redot-engine

Revert "solved ctrl + alt + special character Issue #6851"

scene/gui/text_edit.cpp
@@ -3619,7 +3619,7 @@ void TextEdit::_gui_input(const Ref<InputEvent> &p_gui_input) {
 			return;
 		}
 
-		if (!keycode_handled && (!k->get_command() || (k->get_command() && k->get_alt()))) { // For German keyboards.
+		if (!keycode_handled && !k->get_command()) { // For German keyboards.
 
 			if (k->get_unicode() >= 32) {
 				if (readonly) {

haruki7049/godot

(cherry picked from commit 8b7f582f22bc5bd2ad55b3a4078af03612ec991b)

scene/gui/text_edit.cpp
@@ -3766,7 +3766,7 @@ void TextEdit::_gui_input(const Ref<InputEvent> &p_gui_input) {
 			return;
 		}
 
-		if (!scancode_handled && (!k->get_command() || (k->get_command() && k->get_alt()))) { // For German keyboards.
+		if (!scancode_handled && !k->get_command()) { // For German keyboards.
 
 			if (k->get_unicode() >= 32) {
 