KleirRampage45/redot-engine

Add `border_width` to `ReferenceRect`
Exposes a hidden parameter behind `CanvasItem.draw_rect()`.

scene/gui/reference_rect.cpp
@@ -38,7 +38,7 @@ void ReferenceRect::_notification(int p_what) {
 			return;
 		}
 		if (Engine::get_singleton()->is_editor_hint() || !editor_only) {
-			draw_rect(Rect2(Point2(), get_size()), border_color, false);
+			draw_rect(Rect2(Point2(), get_size()), border_color, false, border_width);
 		}
 	}
 }
@@ -50,6 +50,15 @@ void ReferenceRect::set_border_color(const Color &p_color) {
 
 Color ReferenceRect::get_border_color() const {
 	return border_color;
+}
+
+void ReferenceRect::set_border_width(float p_width) {
+	border_width = MAX(0.0, p_width);
+	update();
+}
+
+float ReferenceRect::get_border_width() const {
+	return border_width;
 }
 
 void ReferenceRect::set_editor_only(const bool &p_enabled) {
@@ -65,14 +74,13 @@ void ReferenceRect::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_border_color"), &ReferenceRect::get_border_color);
 	ClassDB::bind_method(D_METHOD("set_border_color", "color"), &ReferenceRect::set_border_color);
 
+	ClassDB::bind_method(D_METHOD("get_border_width"), &ReferenceRect::get_border_width);
+	ClassDB::bind_method(D_METHOD("set_border_width", "width"), &ReferenceRect::set_border_width);
+
 	ClassDB::bind_method(D_METHOD("get_editor_only"), &ReferenceRect::get_editor_only);
 	ClassDB::bind_method(D_METHOD("set_editor_only", "enabled"), &ReferenceRect::set_editor_only);
 
 	ADD_PROPERTY(PropertyInfo(Variant::COLOR, "border_color"), "set_border_color", "get_border_color");
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "border_width", PROPERTY_HINT_RANGE, "0.0,5.0,0.1,or_greater"), "set_border_width", "get_border_width");
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "editor_only"), "set_editor_only", "get_editor_only");
-}
-
-ReferenceRect::ReferenceRect() {
-	border_color = Color(1, 0, 0);
-	editor_only = true;
 }

haruki7049/godot

(cherry picked from commit aef00021a9c8a51326d6ed6f9a4fc9ddeae175bd)

scene/gui/reference_rect.cpp
@@ -39,7 +39,7 @@ void ReferenceRect::_notification(int p_what) {
 		if (!is_inside_tree())
 			return;
 		if (Engine::get_singleton()->is_editor_hint() || !editor_only)
-			draw_rect(Rect2(Point2(), get_size()), border_color, false);
+			draw_rect(Rect2(Point2(), get_size()), border_color, false, border_width);
 	}
 }
 
@@ -50,6 +50,15 @@ void ReferenceRect::set_border_color(const Color &p_color) {
 
 Color ReferenceRect::get_border_color() const {
 	return border_color;
+}
+
+void ReferenceRect::set_border_width(float p_width) {
+	border_width = MAX(0.0, p_width);
+	update();
+}
+
+float ReferenceRect::get_border_width() const {
+	return border_width;
 }
 
 void ReferenceRect::set_editor_only(const bool &p_enabled) {
@@ -65,14 +74,13 @@ void ReferenceRect::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_border_color"), &ReferenceRect::get_border_color);
 	ClassDB::bind_method(D_METHOD("set_border_color", "color"), &ReferenceRect::set_border_color);
 
+	ClassDB::bind_method(D_METHOD("get_border_width"), &ReferenceRect::get_border_width);
+	ClassDB::bind_method(D_METHOD("set_border_width", "width"), &ReferenceRect::set_border_width);
+
 	ClassDB::bind_method(D_METHOD("get_editor_only"), &ReferenceRect::get_editor_only);
 	ClassDB::bind_method(D_METHOD("set_editor_only", "enabled"), &ReferenceRect::set_editor_only);
 
 	ADD_PROPERTY(PropertyInfo(Variant::COLOR, "border_color"), "set_border_color", "get_border_color");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "border_width", PROPERTY_HINT_RANGE, "0.0,5.0,0.1,or_greater"), "set_border_width", "get_border_width");
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "editor_only"), "set_editor_only", "get_editor_only");
-}
-
-ReferenceRect::ReferenceRect() {
-	border_color = Color(1, 0, 0);
-	editor_only = true;
 }