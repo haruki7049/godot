KleirRampage45/redot-engine

Fix AudioServer Crash when bus count equals 0

servers/audio_server.cpp
@@ -246,6 +246,7 @@ void AudioServer::_driver_process(int p_frames, int32_t *p_buffer) {
 		init_channels_and_buffers();
 	}
 
+	ERR_FAIL_COND_MSG(buses.is_empty() && todo, "AudioServer bus count is less than 1.");
 	while (todo) {
 		if (to_mix == 0) {
 			_mix_step();

haruki7049/godot

(cherry picked from commit ccc375f16334d0eb8d56d5e5f2b248ef1ee55258)

servers/audio_server.cpp
@@ -246,6 +246,7 @@ void AudioServer::_driver_process(int p_frames, int32_t *p_buffer) {
 		init_channels_and_buffers();
 	}
 
+	ERR_FAIL_COND_MSG(buses.empty() && todo, "AudioServer bus count is less than 1.");
 	while (todo) {
 
 		if (to_mix == 0) {