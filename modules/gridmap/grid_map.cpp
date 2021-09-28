KleirRampage45/redot-engine

Fix visibility toggle for baked GridMaps

modules/gridmap/grid_map.cpp
@@ -715,6 +715,10 @@ void GridMap::_update_visibility() {
 			RS::get_singleton()->instance_set_visible(mi.instance, is_visible_in_tree());
 		}
 	}
+
+	for (int i = 0; i < baked_meshes.size(); i++) {
+		RS::get_singleton()->instance_set_visible(baked_meshes[i].instance, is_visible_in_tree());
+	}
 }
 
 void GridMap::_queue_octants_dirty() {

haruki7049/godot

(cherry picked from commit 809d88b925d440fef6cb9b6572315e365f91487e)

modules/gridmap/grid_map.cpp
@@ -746,6 +746,10 @@ void GridMap::_update_visibility() {
 			VS::get_singleton()->instance_set_visible(mi.instance, is_visible_in_tree());
 		}
 	}
+
+	for (int i = 0; i < baked_meshes.size(); i++) {
+		VS::get_singleton()->instance_set_visible(baked_meshes[i].instance, is_visible_in_tree());
+	}
 }
 
 void GridMap::_queue_octants_dirty() {