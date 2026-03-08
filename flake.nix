7aec9af15a26c8a36000dcbbbb4f5f3bf0e96aee
haruki7049 <tontonkirikiri@gmail.com>
2026-03-08 20:58:21 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -187,7 +187,6 @@
 
             # C / C++
             programs.clang-format.enable = true;
-            programs.clang-tidy.enable = true;
 
             # GitHub Actions
             programs.actionlint.enable = true;
